875e465f5683eee7c301009c3bcc1b6f998c0361
Gennady Terekhov <ter.ge@yandex.ru>
2023-07-22 16:06:13 +0300

    в

--- entity/resource.go
+++ entity/resource.go
@@ -1,8 +1,71 @@
 package entity
 
 type Resource struct {
-	Food       int `json:"food"`
-	Wealth     int `json:"wealth"`
-	Production int `json:"production"`
-	Culture    int `json:"culture"`
+	Food           int `json:"food"`
+	TradingAbility int `json:"trading_ability"`
+	Production     int `json:"production"`
+	Culture        int `json:"culture"`
 }
+
+var (
+	Pasture Resource = Resource{
+		Food:           3,
+		TradingAbility: 0,
+		Production:     0,
+		Culture:        0,
+	}
+	Stone Resource = Resource{
+		Food:           0,
+		TradingAbility: 0,
+		Production:     2,
+		Culture:        1,
+	}
+	Metal Resource = Resource{
+		Food:           0,
+		TradingAbility: 1,
+		Production:     2,
+		Culture:        0,
+	}
+	Fruit Resource = Resource{
+		Food:           2,
+		TradingAbility: 1,
+		Production:     0,
+		Culture:        0,
+	}
+	Lake Resource = Resource{
+		Food:           2,
+		TradingAbility: 0,
+		Production:     0,
+		Culture:        1,
+	}
+	Gold Resource = Resource{
+		Food:           0,
+		TradingAbility: 3,
+		Production:     0,
+		Culture:        0,
+	}
+	Silver Resource = Resource{
+		Food:           0,
+		TradingAbility: 2,
+		Production:     0,
+		Culture:        1,
+	}
+	Forest Resource = Resource{
+		Food:           1,
+		TradingAbility: 0,
+		Production:     2,
+		Culture:        0,
+	}
+	Desert Resource = Resource{
+		Food:           0,
+		TradingAbility: 1,
+		Production:     0,
+		Culture:        1,
+	}
+	River Resource = Resource{
+		Food:           1,
+		TradingAbility: 2,
+		Production:     0,
+		Culture:        1,
+	}
+)
--- entity/territory.go
+++ entity/territory.go
@@ -1,11 +1,18 @@
 package entity
 
 type Territory struct {
-	Food       int    `json:"food"`
-	Wealth     int    `json:"wealth"`
-	Production int    `json:"production"`
-	Culture    int    `json:"culture"`
-	Tiles      []Tile `json:"tiles"`
+	Food           int    `json:"food"`
+	TradingAbility int    `json:"trading_ability"`
+	Production     int    `json:"production"`
+	Culture        int    `json:"culture"`
+	Tiles          []Tile `json:"tiles"`
+}
+
+var currencyToIndexMap = map[string]int{
+	"food":            0,
+	"trading_ability": 1,
+	"production":      2,
+	"culture":         3,
 }
 
 func (territory *Territory) GetTotalFood() int {
@@ -14,4 +21,35 @@ func (territory *Territory) GetTotalFood() int {
 		accumulator += territory.Tiles[i].Resource.Food
 	}
 	return accumulator
+}
+
+func (territory *Territory) GetTotalTradingAbility() int {
+	accumulator := 0
+	for i := 0; i < len(territory.Tiles); i++ {
+		accumulator += territory.Tiles[i].Resource.TradingAbility
+	}
+	return accumulator
+}
+
+func (territory *Territory) GetTotalProduction() int {
+	accumulator := 0
+	for i := 0; i < len(territory.Tiles); i++ {
+		accumulator += territory.Tiles[i].Resource.Production
+	}
+	return accumulator
+}
+
+func (territory *Territory) GetTotalCulture() int {
+	accumulator := 0
+	for i := 0; i < len(territory.Tiles); i++ {
+		accumulator += territory.Tiles[i].Resource.Culture
+	}
+	return accumulator
+}
+
+func (territory *Territory) GetTotal(name string) int {
+	accumulator := 0
+
+	return accumulator
+
 }

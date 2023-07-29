e4f5164ca32d9e03d32a6261c82a55b8944dee51
Gennady Terekhov <ter.ge@yandex.ru>
2023-07-29 20:43:00 +0300

    q

--- entity/resource.go
+++ entity/resource.go
@@ -1,6 +1,7 @@
 package entity
 
 type Resource struct {
+	Quantity       int `json:"quantity"` // how many cards of this type are there
 	Food           int `json:"food"`
 	TradingAbility int `json:"trading_ability"`
 	Production     int `json:"production"`
@@ -35,60 +36,70 @@ var ResourceNames = [ResourcesCount]string{
 
 var (
 	Pasture Resource = Resource{
+		Quantity:       10,
 		Food:           3,
 		TradingAbility: 0,
 		Production:     0,
 		Culture:        0,
 	}
 	Stone Resource = Resource{
+		Quantity:       10,
 		Food:           0,
 		TradingAbility: 0,
 		Production:     2,
 		Culture:        1,
 	}
 	Metal Resource = Resource{
+		Quantity:       10,
 		Food:           0,
 		TradingAbility: 1,
 		Production:     2,
 		Culture:        0,
 	}
 	Fruit Resource = Resource{
+		Quantity:       8,
 		Food:           2,
 		TradingAbility: 1,
 		Production:     0,
 		Culture:        0,
 	}
 	Lake Resource = Resource{
+		Quantity:       4,
 		Food:           2,
 		TradingAbility: 0,
 		Production:     0,
 		Culture:        1,
 	}
 	Gold Resource = Resource{
+		Quantity:       2,
 		Food:           0,
-		TradingAbility: 3,
+		TradingAbility: 4,
 		Production:     0,
 		Culture:        0,
 	}
 	Silver Resource = Resource{
+		Quantity:       4,
 		Food:           0,
 		TradingAbility: 2,
 		Production:     0,
 		Culture:        1,
 	}
 	Forest Resource = Resource{
+		Quantity:       20,
 		Food:           1,
 		TradingAbility: 0,
 		Production:     2,
 		Culture:        0,
 	}
 	Desert Resource = Resource{
+		Quantity:       6,
 		Food:           0,
 		TradingAbility: 1,
 		Production:     0,
 		Culture:        1,
 	}
 	River Resource = Resource{
+		Quantity:       6,
 		Food:           1,
 		TradingAbility: 2,
 		Production:     0,

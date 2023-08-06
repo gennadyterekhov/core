4b2dc31dc7b12a2b331deb8f5165ceffb248a608
Gennady Terekhov <ter.ge@yandex.ru>
2023-08-06 13:01:42 +0300

    s

--- entity/situation.go
+++ entity/situation.go
@@ -3,9 +3,10 @@ package entity
 type Situation struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Quantity    int    `json:"quantity"` // how many cards are there
 }
 
-const SituationsCount = 8
+const SituationsCount = 10
 
 const SituationNameForestFire = "Forest Fire"
 const SituationNameFlood = "Flood"
@@ -15,6 +16,8 @@ const SituationNameDesertification = "Desertification"
 const SituationNameDivineShield = "Divine Shield"
 const SituationNameDemilitarization = "Demilitarization"
 const SituationNameColumbus = "Columbus"
+const SituationNamePoliticalInstablility = "Political Instablility"
+const SituationNameNothing = "Nothing"
 
 var SituationNames = [SituationsCount]string{
 	SituationNameForestFire,
@@ -25,39 +28,63 @@ var SituationNames = [SituationsCount]string{
 	SituationNameDivineShield,
 	SituationNameDemilitarization,
 	SituationNameColumbus,
+	SituationNamePoliticalInstablility,
+	SituationNameNothing,
 }
 
 var SituationNameToSituationMap map[string](*Situation) = map[string](*Situation){
 	SituationNameForestFire: &Situation{
 		Name:        SituationNameForestFire,
-		Description: "desc",
+		Description: "Remove " + ResourceNameForest + " tile. If you don't have one - remove one from the next player",
+		Quantity:    4,
 	},
 	SituationNameFlood: &Situation{
 		Name:        SituationNameFlood,
-		Description: "desc",
+		Description: "Remove " + ResourceNameRiver + " tile. If you don't have one - remove one from the next player",
+		Quantity:    5,
 	},
 	SituationNameNessie: &Situation{
 		Name:        SituationNameNessie,
-		Description: "desc",
+		Description: "Remove " + ResourceNameLake + " tile. If you don't have one - remove one from the next player",
+		Quantity:    1,
+	},
+	SituationNameDesertification: &Situation{
+		Name: SituationNameDesertification,
+		Description: "Turn one " +
+			ResourceNamePasture +
+			" tile into " +
+			ResourceNameDesert +
+			". If you don't have one - do so with one from the next player",
+		Quantity: 2,
 	},
 	SituationNameBlessing: &Situation{
 		Name:        SituationNameBlessing,
-		Description: "desc",
-	},
-	SituationNameDesertification: &Situation{
-		Name:        SituationNameDesertification,
-		Description: "desc",
+		Description: "+1 Action this round",
+		Quantity:    2,
 	},
 	SituationNameDivineShield: &Situation{
 		Name:        SituationNameDivineShield,
-		Description: "desc",
+		Description: "You win every fight if defending",
+		Quantity:    2,
 	},
 	SituationNameDemilitarization: &Situation{
 		Name:        SituationNameDemilitarization,
-		Description: "desc",
+		Description: "Nobody can arm next round",
+		Quantity:    5,
 	},
 	SituationNameColumbus: &Situation{
 		Name:        SituationNameColumbus,
-		Description: "desc",
+		Description: "Only you can discover new tiles next round",
+		Quantity:    2,
+	},
+	SituationNamePoliticalInstablility: &Situation{
+		Name:        SituationNamePoliticalInstablility,
+		Description: "Rome is weaker 1000pts next round",
+		Quantity:    2,
+	},
+	SituationNameNothing: &Situation{
+		Name:        SituationNameNothing,
+		Description: "Nothing happens",
+		Quantity:    10,
 	},
 }
